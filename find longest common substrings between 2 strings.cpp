8718730bc0121d39e7f99970af2b8f4b47b37d7f
Ayush-D <31109284+Ayush-D@users.noreply.github.com>
2017-10-06 00:15:09 +0530

    Update find longest common substrings between 2 strings.cpp

--- find longest common substrings between 2 strings.cpp
+++ find longest common substrings between 2 strings.cpp
@@ -1,12 +1,18 @@
+//TRY EXAMPLE OF STRINGS AS "LCLC"  AND  "CLCL"
+
 #include <bits/stdc++.h>
 using namespace std;
+
+//using DP we done this
+//bottom up is good than top down
 
 void seeAndCall(string &s1,string s2){
 
 	int len1=s1.size();	int len2=s2.size();
-	int max=INT_MIN;
+	int max=INT_MIN;//initilaze with min of c++ value
 
-	string result[1000]="";
+	//we could also write string result[1000] = "\0" also initilaize with null value in string array
+	string result[1000]="";	  //this is way to initilaize array of result with empty values.
 
 	int match[50][50];
 	int k=0;
@@ -26,13 +32,13 @@ void seeAndCall(string &s1,string s2){
 				max=match[i][j];
 
 				result[k] = s1.substr(i-max+1,i+1);
-				k++;
+				//k++;	//NO need of this
 
 			}
 		if(match[i][j]==max){
 			k=1;
 			result[k]=s1.substr(i-max+1,i+1);
-			k++;
+			//k++;	//no need for this also
 		}	
 
 			}
