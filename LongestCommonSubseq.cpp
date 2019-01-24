f20a426efc11be555f815f0c2c1f8a863788c095
Ayush <31109284+Ayush-D@users.noreply.github.com>
2019-01-24 19:47:23 +0530

    Created Longest Common Subsequence
    It is recursive code finding the how to do it with DP

---
+++ LongestCommonSubseq.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio          std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+char dp[105][105];
+
+string lcs(string a, string b, int i, int j, string k){
+    if(i == -1 || j == -1){
+        //cout<<"Y";
+        return k;
+    }
+    if()
+    if(a[i] == b[j]){
+        return lcs(a,b,i-1,j-1,a[i]+k);
+    }
+
+    string t1 = lcs(a,b,i-1,j,k);
+    string t2 = lcs(a,b,i,j-1,k);
+    if(t1.size() > t2.size())   return t1;
+    else return t2;
+}
+
+int main(){
+    fastio;
+    string s1,s2;
+    cin>>s1>>s2;
+
+    cout<<lcs(s1,s2,s1.size(),s2.size(),"");
+
+}
