e5dc4bf9d478cd165ee7718af14e9b9f1d65d0e4
Ayush <31109284+Ayush-D@users.noreply.github.com>
2019-03-24 11:25:23 +0530

    Prime Factorization And Number of Divisors

--- DFS/Prime Factorization And NOD.cpp
+++ DFS/Prime Factorization And NOD.cpp
@@ -3,7 +3,8 @@ Its prime factorization is 2^2×3. Is it possible to divide 12 with 5?
 No. Cause the prime factorization of 12 does not contain 5. We can divide N with primes that appear in factorization of N.
 Next, can we divide 12 with 2^3? No. The power of 2 in prime factorization of 12 is 2^2. 
 So we cannot divide 12 with 2^3, since 2^2 is not divisible by 23.
-Take Help from Here: https://forthright48.com/2015/07/number-of-divisors-of-integer.html
+Take Help from Here for NOD: https://forthright48.com/2015/07/number-of-divisors-of-integer.html
+Take Help from Here for SOD: https://forthright48.com/category/cpps/number-theory/page/9 
 
 **********************Prime Factorization of Number (Full Working Code) **********************
 
